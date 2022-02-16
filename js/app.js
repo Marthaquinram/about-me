685a87dbc11a14a456867e4990b441070fe6c5f3
Martha <Marthaquin21@gmail.com>
2022-02-15 22:45:42 -0800

    created Javascript prompt yes or no questions

---
+++ js/app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+console.log('Hi');
+
+alert('Welcome to my About Me page ! ');
+let username = prompt('What is your name?');
+alert('Hello ' + username + '!');
+console.log('Nice to meet you ' + username);
+
+let doILikeSoccer = prompt('Do I like soccer?').toLowerCase();
+
+if (doILikeSoccer.toLowerCase() === 'Yes' || doILikeSoccer.toLowerCase()==='yes') {
+  alert('Yes! You got it right! One of my favorite teams is from the English Premier League called Arsenal FC! ');
+  
+  console.log('Yes, who doesn\'t like soccer?! ');
+ 
+}
+
+else if (doILikeSoccer.toLowerCase() === 'no' || doILikeSoccer.toLowerCase()==='n') {
+
+  console.log('Im a die hard fan!');
+
+  alert('Jokes on you, I LOVE soccer, my brother, dad and I all played soccer together!!');
+
+}
+
+else {
+
+  console.log('Yes, who doesn\'t like soccer?!'); 
+
+}
+
+
+let doILikeAlpacas = prompt('Do I like Alpacas?').toLowerCase();
+
+if (doILikeAlpacas.toLowerCase() === 'Yes' || doILikeAlpacas.toLowerCase()==='yes') {
+  alert('Is it that obvious? They\'re one of my favorite animals, If I ever get to own one, I would name it Chewpaca! Get it? ');
+  
+  console.log('Alpacas are so adorable! ');
+ 
+}
+
+else if (doILikeAlpacas.toLowerCase() === 'no' || doILikeAlpacas.toLowerCase()==='n') {
+
+  console.log('Alpacas are so fluffy, Im gonna die!!');
+
+  alert('Sorry to break it to you BUT wrong, I love alpacas!');
+
+}
+
+
+let doILikeAlpacas = prompt('Do I like Alpacas?').toLowerCase();
+
+if (doILikeAlpacas.toLowerCase() === 'Yes' || doILikeAlpacas.toLowerCase()==='yes') {
+  alert('Is it that obvious? They\'re one of my favorite animals, If I ever get to own one, I would name it Chewpaca! Get it? ');
+  
+  console.log('Alpacas are so adorable! ');
+ 
+}
+
+else if (doILikeAlpacas.toLowerCase() === 'no' || doILikeAlpacas.toLowerCase()==='n') {
+
+  console.log('Alpacas are so fluffy, Im gonna die!!');
+
+  alert('Sorry to break it to you BUT wrong, I love alpacas!');
+
+}
