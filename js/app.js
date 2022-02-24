6186a461e7e6107fbf7faba2cae23fa345755808
Martha <Marthaquin21@gmail.com>
2022-02-23 23:53:17 -0800

    added some notes to my js file for my personal understanding

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ let username = prompt('What is your name?');
 
 alert('Hello ' + username + '!');
 let score = 0;
+let numberOfQuestions = 0;
 
 console.log('Nice to meet you ' + username);
 
@@ -15,7 +16,7 @@ console.log('Nice to meet you ' + username);
 
 function sportQuestion() {
   let doILikeSoccer = prompt('Do I like soccer? Hint: Yes or No ').toLowerCase();
-
+  numberOfQuestions++;
   if ((doILikeSoccer === 'yes') || (doILikeSoccer === 'y')) {
     alert('Yes! You got it right! One of my favorite teams is from the English Premier League called Arsenal FC! ');
     score++;
@@ -39,6 +40,7 @@ sportQuestion();
 
 function favouriteAnimal() {
   let doILikeAlpacas = prompt('Do I like alpacas? Hint: Yes or No ').toLowerCase();
+  numberOfQuestions++;
 
   if ((doILikeAlpacas === 'yes') || (doILikeAlpacas === 'y')) {
     alert('Is it that obvious? They\'re one of my favorite animals, If I ever get to own one, I would name it Chewpaca! Get it? ');
@@ -61,6 +63,7 @@ favouriteAnimal();
 
 function danceQuestion(){
   let doILikeToDance = prompt('Do I like to dance? Hint: Yes or No').toLowerCase();
+  numberOfQuestions++;
 
   if ((doILikeToDance === 'yes') || (doILikeToDance === 'y')) {
     alert('YOU BET I LIKE TO DANCE! Dance is one of my biggest passion, I\'ve been dancing since I was 13 years old! ');
@@ -82,6 +85,7 @@ danceQuestion();
 
 function workOut(){
   let doILikeWorkingOut = prompt('Do I enjoy working out? Hint: Yes or No ').toLowerCase();
+  numberOfQuestions++;
 
   if ((doILikeWorkingOut === 'yes') || (doILikeWorkingOut ==='y')) {
     alert('Even though I dance and play soccer, I am NOT a fan of traditional gyms and working out! ');
@@ -103,6 +107,7 @@ workOut();
 
 function makeupQuestion(){
   let doILikeMakeup = prompt('Do I like doing makeup? Hint: Yes or No ').toLowerCase();
+  numberOfQuestions++;
 
   if ((doILikeMakeup === 'yes') || (doILikeMakeup === 'y')) {
     alert('Another obvious one right? I was a makeup artist for a couple of years and I enjoyed it while it lasted!');
@@ -123,6 +128,7 @@ makeupQuestion();
 function howManyTattoos(){
   let myTattoos = 4;
   let attempts = 4;
+  numberOfQuestions++;
   for (let i = 0; i < attempts; i++) {
     // console.log(`Hello from loop ${i}`);
     let tattoosGuess = prompt('How many tattoos do I have? ');
@@ -142,40 +148,56 @@ function howManyTattoos(){
 
     }
   } console.log('The correct answer is 4');
-} 
+}
 howManyTattoos();
 
 //Question 7
 
 function soccerTeam(){
   let arsenalPlayers = ['bellerin', 'saka', 'smithrowe', 'odegarrd'];
-  let lock = 1;
+  //this is my array, inside of the array has index's starting with bellerin at 0 and saka at 1, smithrowe 2, odegarrd is 3, totaling 4.
+  let stopsign = false;
+  //we dont have a stopsign
   let guessesRemaining = 6;
-  // let victory = false;
-  while (guessesRemaining > 0 && lock === 1){
+  //user has 6 attempts
+  let guessCorrectly = false;
+  //the user is not correct yet, they start out wrong.
+  numberOfQuestions++;
+  //while the guesses remain is 6 its greater an 0 AND while we are not at a stopsign, run the loop
+  while (guessesRemaining > 0 && stopsign === false){
     let guessPlayer = prompt('Who is my favorite player?').toLowerCase();
-    console.log(guessPlayer);
+    //console.log(guessPlayer);
+    //this is asking the user the question
     for(let j = 0; j < arsenalPlayers.length; j++){
       //let player = arsenalPlayers[j];
       //console.log(player);
       if (guessPlayer === arsenalPlayers[j]) {
-        //console.log('Goaaaal, youre correct!');
-        alert(`GOOOOOOOOAL! You're right! He is one of my favorite players!`);
-        // victory = true;
-        guessesRemaining = 0;
+        //if the users input is equal to my first j player then console.log('Goaaaal, youre correct!');
+        alert('GOOOOOOOOAL! You\'re right! He is one of my favorite players!');
         score++;
-        lock = 0;
+        //adding up the score
+        stopsign = true;
+        //this stops the while loop. because the user answered correctly.
+        guessCorrectly = true;
+        //this is if the user answers correctly then this is true.
 
       }
     }
     guessesRemaining--;
+    //this decreases the amount of attempts each time the user guesses incorrectly, and console.logs how many attempts they have left
+    if (guessCorrectly === false){
+      //if the user answers incorrectly then itll alert them that theyre wrong.
+      alert('Thats incorrect!');
+    }
     console.log(`You have ${guessesRemaining}`);
-    // this is referring to guesses remaining
-    if (guessesRemaining === 0){
+    // this displays in the console.log how many guess they decreased.
+    if (guessesRemaining === 0 && guessCorrectly === false){
+      //if the user is out of guess, AND theyve guessed incorrectly then run message below.
       alert(`You're out of guesses! The correct answers were  ${arsenalPlayers}`);
 
     }
   }
 }
 soccerTeam();
-alert('You got ' + score + ' out of 7! ');
+alert(`You got ${score} out of ${numberOfQuestions}`);
+//this is telling the user their score out of how many questions there are.
