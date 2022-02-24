dcf97791df45062aa524b06ef6e887b1b2d37140
Martha <Marthaquin21@gmail.com>
2022-02-23 21:06:41 -0800

    updated my question 7 while and for loop. something broke and fixed it with Zayah and Derek

--- js/app.js
+++ js/app.js
@@ -9,50 +9,50 @@ let username = prompt('What is your name?');
 alert('Hello ' + username + '!');
 let score = 0;
 
-// console.log('Nice to meet you ' + username);
+console.log('Nice to meet you ' + username);
 
-// Question 1
+// // Question 1
 
 function sportQuestion() {
-let doILikeSoccer = prompt('Do I like soccer? Hint: Yes or No ').toLowerCase();
+  let doILikeSoccer = prompt('Do I like soccer? Hint: Yes or No ').toLowerCase();
 
-if ((doILikeSoccer === 'yes') || (doILikeSoccer === 'y')) {
-  alert('Yes! You got it right! One of my favorite teams is from the English Premier League called Arsenal FC! ');
-  score++;
+  if ((doILikeSoccer === 'yes') || (doILikeSoccer === 'y')) {
+    alert('Yes! You got it right! One of my favorite teams is from the English Premier League called Arsenal FC! ');
+    score++;
 
-  // console.log('Yes, who doesn\'t like soccer?! ');
- 
-} else if ((doILikeSoccer === 'no') || (doILikeSoccer ==='n')) {
+    // console.log('Yes, who doesn\'t like soccer?! ');
 
-  console.log('Im a die hard fan!');
+  } else if ((doILikeSoccer === 'no') || (doILikeSoccer ==='n')) {
 
-  alert('Jokes on you, I LOVE soccer, my brother, dad and I all played soccer together!!');
+    console.log('Im a die hard fan!');
 
-} else {
-  console.log('Yes, who doesn\'t like soccer?!'); 
+    alert('Jokes on you, I LOVE soccer, my brother, dad and I all played soccer together!!');
 
-}
+  } else {
+    console.log('Yes, who doesn\'t like soccer?!');
+
+  }
 }
 sportQuestion();
 
 // Question 2
 
 function favouriteAnimal() {
-let doILikeAlpacas = prompt('Do I like alpacas? Hint: Yes or No ').toLowerCase();
+  let doILikeAlpacas = prompt('Do I like alpacas? Hint: Yes or No ').toLowerCase();
 
-if ((doILikeAlpacas === 'yes') || (doILikeAlpacas === 'y')) {
-  alert('Is it that obvious? They\'re one of my favorite animals, If I ever get to own one, I would name it Chewpaca! Get it? ');
-  score++;
+  if ((doILikeAlpacas === 'yes') || (doILikeAlpacas === 'y')) {
+    alert('Is it that obvious? They\'re one of my favorite animals, If I ever get to own one, I would name it Chewpaca! Get it? ');
+    score++;
 
-  // console.log('Alpacas are so adorable! ');
+    // console.log('Alpacas are so adorable! ');
 
-} else if ((doILikeAlpacas === 'no') || (doILikeAlpacas === 'n')) {
+  } else if ((doILikeAlpacas === 'no') || (doILikeAlpacas === 'n')) {
 
-  console.log('Alpacas are so fluffy, Im gonna die!!');
+    console.log('Alpacas are so fluffy, Im gonna die!!');
 
-  alert('Sorry to break it to you BUT wrong, I love alpacas!');
+    alert('Sorry to break it to you BUT wrong, I love alpacas!');
 
-}
+  }
 }
 favouriteAnimal();
 
@@ -60,20 +60,20 @@ favouriteAnimal();
 // Question 3
 
 function danceQuestion(){
-let doILikeToDance = prompt('Do I like to dance? Hint: Yes or No').toLowerCase();
+  let doILikeToDance = prompt('Do I like to dance? Hint: Yes or No').toLowerCase();
 
-if ((doILikeToDance === 'yes') || (doILikeToDance === 'y')) {
-  alert('YOU BET I LIKE TO DANCE! Dance is one of my biggest passion, I\'ve been dancing since I was 13 years old! ');
-  score++;
-  // console.log('Dancing is also a great workout! ');
+  if ((doILikeToDance === 'yes') || (doILikeToDance === 'y')) {
+    alert('YOU BET I LIKE TO DANCE! Dance is one of my biggest passion, I\'ve been dancing since I was 13 years old! ');
+    score++;
+    // console.log('Dancing is also a great workout! ');
 
-} else if ((doILikeToDance === 'no') || (doILikeToDance ==='n')) {
+  } else if ((doILikeToDance === 'no') || (doILikeToDance ==='n')) {
 
-  console.log('BATTLE ME!');
+    console.log('BATTLE ME!');
 
-  alert('Dancing is a big part of my life! BATTLE ME!!');
+    alert('Dancing is a big part of my life! BATTLE ME!!');
 
-}
+  }
 }
 danceQuestion();
 
@@ -81,20 +81,20 @@ danceQuestion();
 // Question 4
 
 function workOut(){
-let doILikeWorkingOut = prompt('Do I enjoy working out? Hint: Yes or No ').toLowerCase();
+  let doILikeWorkingOut = prompt('Do I enjoy working out? Hint: Yes or No ').toLowerCase();
 
-if ((doILikeWorkingOut === 'yes') || (doILikeWorkingOut ==='y')) {
-  alert('Even though I dance and play soccer, I am NOT a fan of traditional gyms and working out! ');
-  score++;
-  // console.log('I dont like working out in gyms! ');
- 
-} else if ((doILikeWorkingOut === 'no') || (doILikeWorkingOut ==='n')) {
+  if ((doILikeWorkingOut === 'yes') || (doILikeWorkingOut ==='y')) {
+    alert('Even though I dance and play soccer, I am NOT a fan of traditional gyms and working out! ');
+    score++;
+    // console.log('I dont like working out in gyms! ');
 
-  console.log('No gym for me!');
+  } else if ((doILikeWorkingOut === 'no') || (doILikeWorkingOut ==='n')) {
 
-  alert('No is correct, I like the work out I get in dance and soccer but Im not a fan of going to a gym and lifting weights!!');
+    console.log('No gym for me!');
 
-}
+    alert('No is correct, I like the work out I get in dance and soccer but Im not a fan of going to a gym and lifting weights!!');
+
+  }
 }
 workOut();
 
@@ -102,68 +102,80 @@ workOut();
 // Question 5
 
 function makeupQuestion(){
-let doILikeMakeup = prompt('Do I like doing makeup? Hint: Yes or No ').toLowerCase();
+  let doILikeMakeup = prompt('Do I like doing makeup? Hint: Yes or No ').toLowerCase();
 
-if ((doILikeMakeup === 'yes') || (doILikeMakeup === 'y')) {
-  alert('Another obvious one right? I was a makeup artist for a couple of years and I enjoyed it while it lasted!');
-  score++;
+  if ((doILikeMakeup === 'yes') || (doILikeMakeup === 'y')) {
+    alert('Another obvious one right? I was a makeup artist for a couple of years and I enjoyed it while it lasted!');
+    score++;
 
-  // console.log('I don\'t wear makeup that much anymore!');
+    // console.log('I don\'t wear makeup that much anymore!');
 
-} else if ((doILikeMakeup === 'no') || (doILikeMakeup === 'n')) {
-  console.log('I enjoy the natural look these days.');
-  
-  alert('Even though I don\'t do makeup that often, I still enjoy it.');
-}
+  } else if ((doILikeMakeup === 'no') || (doILikeMakeup === 'n')) {
+    console.log('I enjoy the natural look these days.');
+
+    alert('Even though I don\'t do makeup that often, I still enjoy it.');
+  }
 }
 makeupQuestion();
 
 // Question 6
 
 function howManyTattoos(){
-let myTattoos = 4;
-let attempts = 4;
-for (let i = 0; i < attempts; i++) {
-  console.log(`Hello from loop ${i}`);
-  let tattoosGuess = prompt('How many tattoos do I have? ');
-  tattoosGuess = parseInt(tattoosGuess);
-  console.log(`You guessed ${tattoosGuess}`);
-  if (tattoosGuess === myTattoos) {
-    console.log('You are correct! WOOH!');
-    score++;
-    break;
-  } else if(tattoosGuess > myTattoos){
-    console.log('Too high my friend!');
-  } else if(tattoosGuess < myTattoos){
-    console.log('Too low, oh nooo!');
+  let myTattoos = 4;
+  let attempts = 4;
+  for (let i = 0; i < attempts; i++) {
+    // console.log(`Hello from loop ${i}`);
+    let tattoosGuess = prompt('How many tattoos do I have? ');
+    tattoosGuess = parseInt(tattoosGuess);
+    console.log(`You guessed ${tattoosGuess}`);
+    if (tattoosGuess === myTattoos) {
+      // console.log('You are correct! WOOH!');
+      alert('Woohoo! You guessed correct!');
+      score++;
+      break;
+    } else if(tattoosGuess > myTattoos){
+      // console.log('Too high my friend!');
+      alert('You\'re too high, my friend!');
+    } else if(tattoosGuess < myTattoos){
+      // console.log('Too low, oh nooo!');
+      alert('Too low, oh noooo!');
 
-  }
-} console.log('The correct answer is 4');
-}
+    }
+  } console.log('The correct answer is 4');
+} 
 howManyTattoos();
 
 //Question 7
 
 function soccerTeam(){
-let arsenalPlayers = ['bellerin', 'saka', 'smithrowe', 'odegarrd'];
-let guessesRemaining = 6;
-let victory = false;
-while (guessesRemaining && !victory){
-  let guessPlayer = prompt('Who is my favorite player?').toLowerCase();
-  guessesRemaining--;
-  console.log(guessPlayer);
-  for(let j = 0; j < arsenalPlayers.length; j++){
-    let player = arsenalPlayers[j];
-    console.log(player);
-    if (guessPlayer === player) {
-      console.log('Goaaaal, youre correct!');
-      victory = true;
-      score++;
+  let arsenalPlayers = ['bellerin', 'saka', 'smithrowe', 'odegarrd'];
+  let lock = 1;
+  let guessesRemaining = 6;
+  // let victory = false;
+  while (guessesRemaining > 0 && lock === 1){
+    let guessPlayer = prompt('Who is my favorite player?').toLowerCase();
+    console.log(guessPlayer);
+    for(let j = 0; j < arsenalPlayers.length; j++){
+      //let player = arsenalPlayers[j];
+      //console.log(player);
+      if (guessPlayer === arsenalPlayers[j]) {
+        //console.log('Goaaaal, youre correct!');
+        alert(`GOOOOOOOOAL! You're right! He is one of my favorite players!`);
+        // victory = true;
+        guessesRemaining = 0;
+        score++;
+        lock = 0;
+
+      }
+    }
+    guessesRemaining--;
+    console.log(`You have ${guessesRemaining}`);
+    // this is referring to guesses remaining
+    if (guessesRemaining === 0){
+      alert(`You're out of guesses! The correct answers were  ${arsenalPlayers}`);
+
     }
   }
-  console.log('You have '+ guessesRemaining);
-  // this is referring to guesses remaining
-}
 }
 soccerTeam();
 alert('You got ' + score + ' out of 7! ');
